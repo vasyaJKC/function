b96a90895a7ca536272d6b7986eb640918db93e1
Vasya <king708070807080@gmail.com>
2024-04-27 22:22:07 +0500

    function/def

---
+++ homework.py
@@ -0,0 +1,33 @@
+def get_first_value(lst):
+    return lst[0]
+print(get_first_value([1,2,3]))
+print(get_first_value([80, 5, 100]))
+print(get_first_value([-500, 0, 50]))
+
+
+
+
+
+def concat(lst1,lst2):
+    concatt = lst1 + lst2
+    return concatt
+print(concat([1, 3, 5], [2, 6, 8]))
+print(concat([7, 8], [10, 9, 1, 1, 2]))
+print(concat([4, 5, 1], [3, 3, 3, 3]))
+
+
+
+
+def get_last_item(lst4):
+    return lst4[-1]
+print(get_last_item([1, 2, 3]))
+print(get_last_item(['cat', 'dog', 'duck']))
+print(get_last_item([True, False, True]))
+print(get_last_item([7, 'Stringg', False]))
+
+
+
+def first(lis):
+    return lis[0]
+ruseult = first([1,1,1,1])
+print(ruseult)
